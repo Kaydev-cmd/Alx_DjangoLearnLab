053670e1b7f02345eb909c48fc9cd4211acc4bca
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-11-09 19:51:33 +0200

    feat: add function-based and class-based views with templates and URL routing

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.view.generic import DetailView
+from .models import Book, Library
 
 # Create your views here.
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/book_list.html', {'books': books})
+
+
+def LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
